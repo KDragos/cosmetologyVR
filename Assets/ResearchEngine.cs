6822d021f1acf598bc30a6d7ab713b425c49a835
Kristin Dragos <KristinDragos@gmail.com>
2017-03-04 19:58:33 -0800

    Added an exit canvas that comes up when clicking the door handle.
    FEATURE: Updated lightmaps.
    FEATURE: When clicking the door, a canvas appears with options to leave
    the game or go back to the salon.

---
+++ Assets/ResearchEngine.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchEngine : MonoBehaviour {
+	public AudioSource audioSource;
+	public AudioClip[] audioClips;
+	public Canvas exitCanvas;
+	private Dictionary<string, AudioClip> research;
+
+	void Start () {
+		exitCanvas.enabled = false;
+		SetupDictionary ();
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	public void Play(string name) {
+		if (research.ContainsKey (name)) {
+			audioSource.clip = research [name];
+			audioSource.Play();
+		}
+	}
+
+	public void Stop () {
+		audioSource.Stop();
+	}
+
+	public void Door() {
+		if (exitCanvas == null) {
+			Debug.Log ("canvas is null");
+		}
+		if (exitCanvas.enabled) {
+			Debug.Log ("Canvas is enabled");
+		}
+		if (!exitCanvas.enabled) {
+			Debug.Log ("canvas isn't enabled.");
+		}
+		exitCanvas.enabled = true;
+		if (exitCanvas.enabled) {
+			Debug.Log ("Canvas is enabled");
+		}
+		if (!exitCanvas.enabled) {
+			Debug.Log ("canvas isn't enabled.");
+		}
+		Play ("Goodbye");
+	}
+
+	public void DoorCancel() {
+		exitCanvas.enabled = false;
+	}
+
+	public void DoorQuit() {
+		Debug.Log ("Quitting game.");
+		Application.Quit ();
+	}
+
+	private void SetupDictionary() {
+		research = new Dictionary<string, AudioClip> ();
+		research.Add ("Welcome", audioClips [6]);
+		research.Add ("Reception", audioClips [3]);
+		research.Add ("Styling1", audioClips [4]);
+		research.Add ("Shampoo", audioClips [5]);
+		research.Add ("Dispensary", audioClips [0]);
+		research.Add ("BreakRoom", audioClips [1]);
+		research.Add ("Nails", audioClips [2]);
+		research.Add ("Goodbye", audioClips [7]);
+
+	}
+}
--- Assets/WaypointNavigation.cs
+++ Assets/WaypointNavigation.cs
@@ -6,6 +6,10 @@ public class WaypointNavigation : MonoBehaviour {
 	public Material materialNormal;
 	public Material materialHover;
 	public GameObject player;
+	public GameObject researchEngine;
+	public Canvas canvas;
+
+	private ResearchEngine engine;
 
 	private Renderer renderer;
 	public float speed;
@@ -13,6 +17,8 @@ public class WaypointNavigation : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		renderer = gameObject.GetComponent<Renderer> ();
+		engine = researchEngine.GetComponent<ResearchEngine> ();
+		canvas.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -33,24 +39,37 @@ public class WaypointNavigation : MonoBehaviour {
 
 	public void onClick() {
 		// On click, move player to the targetted waypoint using iTween.
-
 		iTween.MoveTo(player, iTween.Hash(
 			"x", gameObject.transform.position.x,
 			"z", gameObject.transform.position.z,
 			"time", speed
 		));
-
+			
 	}
 
 	void OnTriggerEnter(Collider col) {
-		if (col.tag == "player") {
+		if (col.tag.Equals("Player")) {
 			renderer.enabled = false;
+			engine.Play (gameObject.name);
+			// Activate the waypoint's canvas if it has one. 
+			Debug.Log("Starting canvas status: " + canvas.enabled.ToString());
+			if (canvas != null) {
+				canvas.enabled = true;
+			}
+			Debug.Log("Ending canvas status: " + canvas.enabled.ToString());
+
 		}
 	}
 
 	void OnTriggerExit(Collider col) {
-		if (col.tag == "player") {
+		if (col.tag.Equals("Player")) {
 			renderer.enabled = true;	
+			engine.Stop ();
+			// Deactivate canvas if it has one.
+			if (canvas != null) {
+				canvas.enabled = false;
+			}
+
 		}
 	}
 }
