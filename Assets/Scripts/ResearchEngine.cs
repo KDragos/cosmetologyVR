b494d7be643d1fa53b29f14e370dbf88cf06f102
Kristin Dragos <KristinDragos@gmail.com>
2017-03-05 16:43:34 -0800

    Adjusted lighting and cleaned up project.

---
+++ Assets/Scripts/ResearchEngine.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchEngine : MonoBehaviour {
+	public AudioSource audioSource;
+	public AudioClip[] audioClips;
+	public Canvas exitCanvasFront;
+	public Canvas exitCanvasBack;
+	private Dictionary<string, AudioClip> research;
+
+	void Start () {
+		SetCanvasesEnabled (false);
+		SetupDictionary ();
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	public void Play(string name) {
+		if (research.ContainsKey (name)) {
+			audioSource.clip = research [name];
+			audioSource.Play();
+		}
+	}
+
+	public void Stop () {
+		audioSource.Stop();
+	}
+
+	public void Door() {
+		SetCanvasesEnabled (true);
+		Play ("Goodbye");
+	}
+
+	public void DoorCancel() {
+		SetCanvasesEnabled (false);
+
+	}
+
+	public void DoorQuit() {
+		Debug.Log ("Quitting game.");
+		Application.Quit ();
+	}
+
+	private void SetCanvasesEnabled(bool status) {
+		exitCanvasFront.enabled = status;
+		exitCanvasBack.enabled = status;
+	}
+
+	private void SetupDictionary() {
+		research = new Dictionary<string, AudioClip> ();
+		research.Add ("Welcome", audioClips [6]);
+		research.Add ("Reception", audioClips [3]);
+		research.Add ("Styling1", audioClips [4]);
+		research.Add ("Shampoo", audioClips [5]);
+		research.Add ("Dispensary", audioClips [0]);
+		research.Add ("BreakRoom", audioClips [1]);
+		research.Add ("Nails", audioClips [2]);
+		research.Add ("Goodbye", audioClips [7]);
+
+	}
+}
---
+++ Assets/Scripts/WaypointNavigation.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNavigation : MonoBehaviour {
+	public Material materialNormal;
+	public Material materialHover;
+	public GameObject player;
+	public GameObject researchEngine;
+	public Canvas canvas;
+
+	private ResearchEngine engine;
+
+	private Renderer renderer;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<Renderer> ();
+		engine = researchEngine.GetComponent<ResearchEngine> ();
+		canvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Check to render the point.
+	}
+
+	public void onStartHover() {
+		// On hover, change material to the darker less translucent material. 
+		renderer.material = materialHover;
+
+	}
+
+	public void onEndHover() {
+		// on hover exit, change material to lighter material.
+		renderer.material = materialNormal;
+	}
+
+	public void onClick() {
+		// On click, move player to the targetted waypoint using iTween.
+		iTween.MoveTo(player, iTween.Hash(
+			"x", gameObject.transform.position.x,
+			"z", gameObject.transform.position.z,
+			"time", speed
+		));
+			
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag.Equals("Player")) {
+			renderer.enabled = false;
+			engine.Play (gameObject.name);
+			// Activate the waypoint's canvas if it has one. 
+			if (canvas != null) {
+				canvas.enabled = true;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.tag.Equals("Player")) {
+			renderer.enabled = true;
+			engine.Stop ();
+			// Deactivate canvas if it has one.
+			if (canvas != null) {
+				canvas.enabled = false;
+			}
+
+		}
+	}
+}
